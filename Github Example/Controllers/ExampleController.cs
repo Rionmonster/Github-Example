d5ef860eeec1e1312330cd44562ffcbfcb127520
Rion <rionmonster@gmail.com>
2013-04-19 21:53:15 -0500

    Initial Commit

---
+++ Github Example/Controllers/ExampleController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Github_Example.Controllers
+{
+    public class ExampleController : Controller
+    {
+        //
+        // GET: /Example/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
